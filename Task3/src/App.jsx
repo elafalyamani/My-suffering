aeff024639050f21d4c94d1aeb630c6f08c3c0ea
Elaf Alyamany <alyamanyelaf@gmail.com>
2026-05-15 07:55:44 +0300

    Finished task 3

---
+++ Task3/src/App.jsx
@@ -0,0 +1,17 @@
+import Counter from './Counter'
+import Unicafe from './Unicafe'
+import Anecdotes from './Anecdotes'
+import Todo from './Todo'
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+      <Unicafe />
+      <Anecdotes />
+      <Todo />
+    </div>
+  )
+}
+
+export default App
